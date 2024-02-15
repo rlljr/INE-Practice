3772c15dadde37990225868e56ccaa1099c8c1d6
rlimlatjr <raullatjr@gmail.com>
2024-02-15 11:50:23 -0800

    Commit

---
+++ reverse_a_list.py
@@ -0,0 +1,11 @@
+# Reverse a list
+# Write a function that takes in a list and returns the list reversed.
+#
+# reverse_a_list([1, 2, 3, 4]) == [4, 3, 2, 1]
+
+def reverse_a_list(list):
+    return list[::-1]
+    pass
+
+
+print(reverse_a_list([1,2,3,4,5,6,7]))
