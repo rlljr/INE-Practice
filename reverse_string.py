14b86c8bb6009d5592ae723cab5ba2c3e93510ae
rlimlatjr <raullatjr@gmail.com>
2024-02-14 22:39:56 -0800

    Commit

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+#Write a function reverse_string that receives a string and returns it reversed. Example:
+# reverse_string('Hello World')  # 'dlroW olleH'
+# reverse_string('Python')  # nohtyP
+
+def reverse_string(a_string):
+    return a_string[::-1]
+    pass
+
+print(reverse_string('Hello World asdfsafdsdfsdf'))
