f38fba35b649fb3dbc8970241b102717661f4610
rlimlatjr <raullatjr@gmail.com>
2024-02-15 15:06:08 -0800

    Commit

--- get_position_of_char.py
+++ get_position_of_char.py
@@ -3,7 +3,31 @@
 #
 # get_positions_of_char("aabcda", "a") # Returns "0,1,5,"
 # Hint: You might have to put str() around to position to change it from an integer into a string so you can add it to your result string.
+#
+# def get_positions_of_char(another_string, char_being_searched_for):
+#
+#     pass
 
 def get_positions_of_char(another_string, char_being_searched_for):
 
+    my_string =""
+    for count, ele in enumerate(another_string):
+        if ele == char_being_searched_for:
+            my_string+=str(count)+","
+    return my_string
     pass
+
+# def chars_in_even_positions(a_string):
+#     even_position = []
+#     for i in range(len(a_string)):
+#         if (i%2 == 0):
+#             even_position.append(a_string[i])
+#             new_string = "".join(str(e) for e in even_position)
+#             #print(new_string)
+#         else:
+#             continue
+#     return new_string
+#
+#     pass
+
+print(get_positions_of_char("asdasdasd","x"))
